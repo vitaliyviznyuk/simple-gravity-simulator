0c75cf6f24402661e9fae69173f70bc24a2f9f8c
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-05-11 22:34:25 +0300

    Created an instance of the nBodyProblem

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+/**
+ * Gravitational n-body algorithm
+ */
 class nBodyProblem {
     constructor(params) {
         this.g = params.g;
@@ -25,6 +28,7 @@ class nBodyProblem {
 
         for (let i = 0; i < massesLen; i++) {
             const massI = this.masses[i];
+
             massI.vx += massI.ax * this.dt;
             massI.vy += massI.ay * this.dt;
             massI.vz += massI.az * this.dt;
@@ -60,6 +64,74 @@ class nBodyProblem {
             massI.ay = ay;
             massI.az = az;
         }
+
         return this;
     }
 }
+
+/*
+ * Inputs for our nBodyProblem
+ */
+
+const g = 39.5;
+const dt = 0.008; // 0.008 years is equal to 2.92 days
+const softeningConstant = 0.15;
+
+const masses = [{
+    name: "Sun", // We use solar masses as the unit of mass, so the mass of the Sun is exactly 1
+    m: 1,
+    x: -1.50324727873647e-6,
+    y: -3.93762725944737e-6,
+    z: -4.86567877183925e-8,
+    vx: 3.1669325898331e-5,
+    vy: -6.85489559263319e-6,
+    vz: -7.90076642683254e-7
+}, {
+    name: "Mercury",
+    m: 1.65956463e-7,
+    x: -0.346390408691506,
+    y: -0.272465544507684,
+    z: 0.00951633403684172,
+    vx: 4.25144321778261,
+    vy: -7.61778341043381,
+    vz: -1.01249478093275
+}, {
+    name: "Venus",
+    m: 2.44699613e-6,
+    x: -0.168003526072526,
+    y: 0.698844725464528,
+    z: 0.0192761582256879,
+    vx: -7.2077847105093,
+    vy: -1.76778886124455,
+    vz: 0.391700036358566
+}, {
+    name: "Earth",
+    m: 3.0024584e-6,
+    x: 0.648778995445634,
+    y: 0.747796691108466,
+    z: -3.22953591923124e-5,
+    vx: -4.85085525059392,
+    vy: 4.09601538682312,
+    vz: -0.000258553333317722
+}, {
+    name: "Mars",
+    m: 3.213e-7,
+    x: -0.574871406752105,
+    y: -1.395455041953879,
+    z: -0.01515164037265145,
+    vx: 4.9225288800471425,
+    vy: -1.5065904473191791,
+    vz: -0.1524041758922603
+}];
+
+/*
+ * Create an instance of the nBodyProblem with the inputs above. We clone the masses array by parsing a stringified
+ * version of it so that we can reset the simulator with a minimum amount of fuss.
+ */
+
+const innerSolarSystem = new nBodyProblem({
+    g,
+    dt,
+    masses: JSON.parse(JSON.stringify(masses)),
+    softeningConstant
+});
