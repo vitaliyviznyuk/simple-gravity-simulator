354d3c721f18f48185ca08b523c482b78d527736
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-05-11 22:48:23 +0300

    Creating a visual display of celestial bodies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-/**
+/*
  * Gravitational n-body algorithm
  */
+
 class nBodyProblem {
     constructor(params) {
         this.g = params.g;
@@ -135,3 +136,58 @@ const innerSolarSystem = new nBodyProblem({
     masses: JSON.parse(JSON.stringify(masses)),
     softeningConstant
 });
+
+/*
+ * Motion trails
+ */
+
+class Manifestation {
+    constructor(ctx, trailLength, radius) {
+        this.ctx = ctx;
+        this.trailLength = trailLength;
+        this.radius = radius;
+        this.positions = [];
+    }
+
+    storePosition(x, y) {
+        this.positions.push({
+            x,
+            y
+        });
+
+        if (this.positions.length > this.trailLength)
+            this.positions.shift();
+    }
+
+    draw(x, y) {
+        this.storePosition(x, y);
+
+        const positionsLen = this.positions.length;
+
+        for (let i = 0; i < positionsLen; i++) {
+            let transparency;
+            let circleScaleFactor;
+
+            const scaleFactor = i / positionsLen;
+
+            if (i === positionsLen - 1) {
+                transparency = 1;
+                circleScaleFactor = 1;
+            } else {
+                transparency = scaleFactor / 2;
+                circleScaleFactor = scaleFactor;
+            }
+
+            this.ctx.beginPath();
+            this.ctx.arc(
+                this.positions[i].x,
+                this.positions[i].y,
+                circleScaleFactor * this.radius,
+                0,
+                2 * Math.PI
+            );
+            this.ctx.fillStyle = `rgb(0, 12, 153, ${transparency})`;
+            this.ctx.fill();
+        }
+    }
+}
